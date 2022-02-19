1aeb689a07b267c5cc87e80a19fafce4636eb3d1
Kvazar3S273 <ba2h007@gmail.com>
2022-02-19 23:51:15 +0200

    Добавьте файлы проекта.

---
+++ React3x4/Entities/Identity/AppUser.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace React3x4.Entities.Identity
+{
+    public class AppUser:IdentityUser<long>
+    {
+        public virtual ICollection<AppUserRole> UserRoles { get; set; }
+
+    }
+}
---
+++ React3x4/Program.cs
@@ -0,0 +1,28 @@
+using React3x4.EFContext;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllersWithViews();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+//app.AddDbContext<EFContext>(options =>
+//options.UseSqlite(Configuration.GetConnectionString("DefaultConnection")));
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller}/{action=Index}/{id?}");
+
+app.MapFallbackToFile("index.html"); ;
+
+app.Run();

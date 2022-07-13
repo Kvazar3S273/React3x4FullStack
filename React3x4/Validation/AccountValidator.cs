b26a9d88d42f8d97f5e79e5c961a8aeabb5d9056
Kvazar3S273 <ba2h007@gmail.com>
2022-07-13 21:51:13 +0300

    create validation

---
+++ React3x4/Validation/AccountValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+using React3x4.Models;
+
+namespace React3x4.Validation
+{
+    public class AccountValidator : AbstractValidator<RegisterViewModel>
+    {
+        public AccountValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotEmpty()
+                .WithMessage("Поле Email не може бути порожнім")
+                .MinimumLength(6)
+                .EmailAddress()
+                .WithMessage("Помилка заповнення поля Email");
+            RuleFor(x => x.Password)
+                .NotEmpty()
+                .WithMessage("Поле Password не може бути порожнім")
+                .MinimumLength(5)
+                .WithMessage("Пароль не може бути коротший, ніж 5 символів")
+                .Matches(@"\d")
+                .WithName("Password")
+                .WithMessage("Пароль повинен містити хоча б одну цифру");
+            RuleFor(x => x.ConfirmPassword)
+                .NotEmpty()
+                .WithMessage("Поле ConfirmPassword не може бути порожнім")
+                .Equal(x => x.Password)
+                .WithMessage("Введене підтвердження не співпадає з паролем");
+
+        }
+    }
+}

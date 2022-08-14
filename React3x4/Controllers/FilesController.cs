9b92a3ace502e6d1d46c11913d7e960f68ec7810
Kvazar3S273 <ba2h007@gmail.com>
2022-08-14 15:39:12 +0300

    create files controller

---
+++ React3x4/Controllers/FilesController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.StaticFiles;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace React3x4.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FilesController : ControllerBase
+    {
+        [HttpGet]
+        [Route("getfile/{name}")]
+        public async Task<IActionResult> DownloadFile([FromRoute] string name)
+        {
+
+            string contentType = "";
+            const string DefaultContentType = "application/octet-stream";
+            string file = Directory.GetCurrentDirectory() +
+                $"/data/{name}";
+
+            var provider = new FileExtensionContentTypeProvider();
+
+            if (!provider.TryGetContentType(file, out contentType))
+            {
+                contentType = DefaultContentType;
+            }
+
+            var bytes = await System.IO.File.ReadAllBytesAsync(file);
+            return File(bytes, contentType, Path.GetFileName(file));
+        }
+    }
+}
--- React3x4/Controllers/FndController.cs
+++ React3x4/Controllers/FndController.cs
@@ -1,8 +1,10 @@
 ﻿using AutoMapper;
 using DataLib;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using React3x4.Constants;
 using React3x4.Mapper.MapperModels;
 using React3x4.Models;
 using System;
@@ -13,6 +15,7 @@ namespace React3x4.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class FndController : ControllerBase
     {
         private readonly AppEFContext _context;
@@ -22,9 +25,9 @@ namespace React3x4.Controllers
         {
             _context = context;
             _mapper = mapper;
-
         }
 
+        [AllowAnonymous]
         [HttpGet]
         public async Task<IActionResult> GetFndsList()
         {
@@ -36,6 +39,7 @@ namespace React3x4.Controllers
             return Ok(fndsList);
         }
 
+        [Authorize(Roles = Roles.Admin)]
         [HttpGet]
         [Route("fnd/{id}")]
         public async Task<IActionResult> GetFndsById(int id)
@@ -53,11 +57,12 @@ namespace React3x4.Controllers
             {
                 return BadRequest(new { message = ex.Message.ToString() });
             }
-
         }
 
+        [Authorize(Roles = Roles.Admin)]
         [HttpPut]
         [Route("fndedit/{id}")]
+        [Authorize(Roles = Roles.Admin)]
         public async Task<IActionResult> EditFndsById(int id, [FromBody] EditFndViewModel model)
         {
             try
